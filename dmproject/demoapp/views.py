57c38e1df438cd0b20ba2c3af34de2553e78fdd0
saheerabanu <saheera95@gmail.com>
2023-12-23 01:10:45 +0530

    added

---
+++ dmproject/demoapp/views.py
@@ -0,0 +1,9 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place
+from . models import People
+# Create your views here.
+def demo(request):
+    obj = Place.objects.all()
+    var = People.objects.all()
+    return render(request,"index.html",{'result':obj,'res':var})
